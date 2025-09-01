2dc36754007433ebcb06039f86c04a6d23a12f3c
Freyder Diaz <freyderjapo@gmail.com>
2025-09-01 12:53:05 -0500

    CRUD clientes Terminado

--- back-end(Django)/Gestor/settings.py
+++ back-end(Django)/Gestor/settings.py
@@ -31,16 +31,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'clientes',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Terceros
+    "rest_framework",
+    "corsheaders",
+    # apps
+    'clientes',
 ]
-
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CORS_ALLOW_ALL_ORIGINS = True
 ROOT_URLCONF = 'Gestor.urls'
 
 TEMPLATES = [
--- back-end(Django)/clientes/views.py
+++ back-end(Django)/clientes/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Clientes
+from .serializers import ClienteSerializer
 
 # Create your views here.
+class ClienteViewSet(viewsets.ModelViewSet):
+    queryset = Clientes.objects.all()
+    serializer_class = ClienteSerializer
